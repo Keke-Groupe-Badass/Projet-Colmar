1e525f2f118cfe5d51698adf9f91bc2ecbaa98cf
romain327 <romain.brouard37@petalmail.com>
2022-12-01 18:52:07 +0100

    ajout de la javadoc pour Lettrine, ajout des deux classes manquantes dans ProjetColmar du dossier code

---
+++ code/ProjetColmar/src/main/java/Whole/AuteurDAO.java
@@ -0,0 +1,5 @@
+package Whole;
+
+public class AuteurDAO extends AbstractDao {
+
+}
---
+++ code/ProjetColmar/src/main/java/Whole/Metadonnee.java
@@ -0,0 +1,62 @@
+package Whole;
+
+public class Metadonnee {
+
+    private Lettrine lettrine;
+    private String nom;
+
+    private int id;
+
+    private String entree;
+
+    private String unite;
+
+    private String description;
+
+    private String getNom() {
+        return nom;
+    }
+
+    private int getId() {
+        return id;
+    }
+
+    private String getEntree() {
+        return entree;
+    }
+
+    private String getUnite() {
+        return unite;
+    }
+
+    private String getDescription() {
+        return description;
+    }
+
+    private void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setEntree(String entree) {
+        this.entree = entree;
+    }
+
+    private void setUnite(String unite) {
+        this.unite = unite;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+    * @param meta
+    */
+    public void Modifier(Metadonnee meta) {
+
+    }
+}
