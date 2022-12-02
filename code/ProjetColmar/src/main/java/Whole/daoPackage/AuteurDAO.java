3b61b79a3ea2408106114a37bcefdf4332cdd0fb
Kaputchino <andreasmulard@gmail.com>
2022-12-02 18:23:37 +0100

    auteurfao docu

--- code/ProjetColmar/src/main/java/Whole/daoPackage/AuteurDAO.java
+++ code/ProjetColmar/src/main/java/Whole/daoPackage/AuteurDAO.java
@@ -1,7 +1,49 @@
 package Whole.daoPackage;
 
-import Whole.daoPackage.AbstractDAO;
+import Whole.ccmsPackage.Auteur;
+
+import java.sql.Connection;
+
+import java.util.ArrayList;
 
 public class AuteurDAO extends AbstractDAO {
+    private static Connection cn;
 
+    /**
+     * Met à jour la BD
+     * @param objet Auteur à changer
+     * @param changement Auteur de changement (les paramètres null ne sont pas à changer)
+     */
+
+
+    public void modifier(Auteur objet , Auteur changement ) {
+
+    }
+    /**
+     * Supprime de la db un Auteur
+     * @param objet un Auteur d'un type à déterminer dans chaque implémentation
+     * @see Auteur
+     */
+
+    public void supprimer(Auteur objet ) {
+
+    }
+    /**
+     * Ajoute à la base de donnée un Auteur
+     * @param donne le Auteur à ajouter
+     * @see Auteur
+     */
+
+    public void creer(Auteur donne) {
+
+    }
+    /**
+     *Cherche un Auteur dans la base
+     * @param donne Auteur avec tout les paramètres nuls sauf ceux à chercher
+     * @see Auteur
+     */
+
+    public ArrayList<Auteur> chercher(Auteur donne) {
+        return null;
+    }
 }
