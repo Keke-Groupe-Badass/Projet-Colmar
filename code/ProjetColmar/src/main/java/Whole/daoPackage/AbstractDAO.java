6bb7f4f11bafa1a40f05c6a60eec64108c269f82
Kaputchino <andreasmulard@gmail.com>
2022-12-09 10:54:13 +0100

    Changement single connection to multiple connection

--- code/ProjetColmar/src/main/java/Whole/daoPackage/AbstractDAO.java
+++ code/ProjetColmar/src/main/java/Whole/daoPackage/AbstractDAO.java
@@ -10,47 +10,54 @@ import java.util.*;
  * modifier, creer, chercher et supprimer des CCMS
  */
 public abstract class AbstractDAO {
-    private  Connection cn;
 
-    public AbstractDAO(Connection cn) {
-        this.cn = cn;
+    public AbstractDAO() {
     }
 
     /**
      * Met à jour la BD
     * @param objet CCMS à changer
     * @param changement CCMS de changement (les paramètres null ne sont pas à changer)
+     * @param cn La connection à la base de donnée
+     * @see CCMS
+     * @see Whole.LinkToDb
     */
 
 
-    public void modifier(CCMS objet , CCMS changement ) {
+    public void modifier(CCMS objet , CCMS changement ,Connection cn) {
 
     }
     /**
      * Supprime de la db un CCMS
     * @param objet un CCMS d'un type à déterminer dans chaque implémentation
+     * @param cn La connection à la base de donnée
      * @see CCMS
+     * @see Whole.LinkToDb
     */
 
-    public void supprimer(CCMS objet ) {
+    public void supprimer(CCMS objet ,Connection cn) {
 
     }
     /**
      * Ajoute à la base de donnée un CCMS
     * @param donne le CCMS à ajouter
+     *@param cn La connection à la base de donnée
      * @see CCMS
+     * @see Whole.LinkToDb
     */
 
-    public void creer(CCMS donne) {
+    public void creer(CCMS donne,Connection cn) {
 
     }
     /**
      *Cherche un CCMS dans la base
     * @param donne CCMS avec tout les paramètres nuls sauf ceux à chercher
+     *@param cn La connection à la base de donnée
      * @see CCMS
+     * @see Whole.LinkToDb
     */
 
-    public ArrayList<CCMS> chercher(CCMS donne) {
+    public ArrayList<CCMS> chercher(CCMS donne,Connection cn) {
         return null;
     }
 
--- code/ProjetColmar/src/main/java/Whole/daoPackage/TagDAO.java
+++ code/ProjetColmar/src/main/java/Whole/daoPackage/TagDAO.java
@@ -5,6 +5,7 @@
 */
 package Whole.daoPackage;
 
+import Whole.LinkToDb;
 import Whole.ccmsPackage.Tag;
 
 import java.sql.Connection;
@@ -14,61 +15,60 @@ import java.util.ArrayList;
  * @see Tag
  */
 public class TagDAO extends AbstractDAO {
-    private static Connection cn;
-    /**
-     * Constructeur de la classe OuvrageDAO. Instancie l'objet Connection
-     * cn avec le cn passe en parametre.
-     *
-     * @param cn objet Connection provenant de SingleConnection
-     * @see Connection
-     * @see Whole.SingleConnection
-     */
-    public TagDAO(Connection cn) {
-        super(cn);
-    }
 
     /**
-     * Constructeur de la classe TagDAO
+     * Constructeur de la classe OuvrageDAO.
+     * @see LinkToDb
      */
     public TagDAO() {
+        super();
     }
+
 
     /**
      * Met à jour la BD
      * @param objet Tag à changer
      * @param changement Tag de changement (les paramètres null ne sont pas à changer)
+     * @param cn La connection à la base de donnée
+     * @see LinkToDb
      */
 
 
 
-    public void modifier(Tag objet , Tag changement ) {
+    public void modifier(Tag objet , Tag changement ,Connection cn) {
 
     }
     /**
      * Supprime de la db un Tag
      * @param objet un Tag d'un type à déterminer dans chaque implémentation
+     * @param cn La connection à la base de donnée
+     * @see LinkToDb
      * @see Tag
      */
 
-    public void supprimer(Tag objet ) {
+    public void supprimer(Tag objet ,Connection cn) {
 
     }
     /**
      * Ajoute à la base de donnée un Tag
      * @param donne le Tag à ajouter
+     * @param cn La connection à la base de donnée
+     * @see LinkToDb
      * @see Tag
      */
 
-    public void creer(Tag donne) {
+    public void creer(Tag donne,Connection cn) {
 
     }
     /**
      *Cherche un Tag dans la base
      * @param donne Tag avec tout les paramètres nuls sauf ceux à chercher
+     * @param cn La connection à la base de donnée
+     * @see LinkToDb
      * @see Tag
      */
 
-    public ArrayList<Tag> chercher(Tag donne) {
+    public ArrayList<Tag> chercher(Tag donne,Connection cn) {
         return null;
     }
 }
