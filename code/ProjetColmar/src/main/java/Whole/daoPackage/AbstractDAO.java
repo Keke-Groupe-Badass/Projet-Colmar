7cd1da682f25d4486c303fd57f9b3b9e3ae6728a
romain327 <romain.brouard37@petalmail.com>
2022-12-10 16:55:27 +0100

    abstractDAO générique

--- code/ProjetColmar/src/main/java/Whole/daoPackage/AbstractDAO.java
+++ code/ProjetColmar/src/main/java/Whole/daoPackage/AbstractDAO.java
@@ -9,48 +9,48 @@ import java.util.*;
  * Classe abstraite servant à factoriser à lié à la base de donnée les méthodes
  * modifier, creer, chercher et supprimer des CCMS
  */
-public abstract class AbstractDAO {
+public abstract class AbstractDAO <CCMS>{
 
     public AbstractDAO() {
     }
 
     /**
      * Met à jour la BD
-    * @param objet CCMS à changer
-    * @param changement CCMS de changement (les paramètres null ne sont pas à changer)
+     * @param objet CCMS à changer
+     * @param changement CCMS de changement (les paramètres null ne sont pas à changer)
      * @param cn La connection à la base de donnée
      * @see CCMS
      * @see Whole.LinkToDb
-    */
+     */
 
 
     public abstract void modifier(CCMS objet , CCMS changement ,Connection cn);
     /**
      * Supprime de la db un CCMS
-    * @param objet un CCMS d'un type à déterminer dans chaque implémentation
+     * @param objet un CCMS d'un type à déterminer dans chaque implémentation
      * @param cn La connection à la base de donnée
      * @see CCMS
      * @see Whole.LinkToDb
-    */
+     */
 
     public abstract void supprimer(CCMS objet ,Connection cn);
     /**
      * Ajoute à la base de donnée un CCMS
-    * @param donne le CCMS à ajouter
+     * @param donne le CCMS à ajouter
      *@param cn La connection à la base de donnée
      * @see CCMS
      * @see Whole.LinkToDb
-    */
+     */
 
     public abstract void creer(CCMS donne,Connection cn);
     /**
      *Cherche un CCMS dans la base
-    * @param donne CCMS avec tout les paramètres nuls sauf ceux à chercher
+     * @param donne CCMS avec tout les paramètres nuls sauf ceux à chercher
      *@param cn La connection à la base de donnée
      * @return la Liste des des CCMS correspondant aux critères
      * @see CCMS
      * @see Whole.LinkToDb
-    */
+     */
 
     public abstract ArrayList<CCMS> chercher(CCMS donne,Connection cn);
 
