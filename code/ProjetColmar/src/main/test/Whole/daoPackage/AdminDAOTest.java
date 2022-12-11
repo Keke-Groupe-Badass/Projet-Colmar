8ae5eae93a5f2d31d256948358a2c86b87fa9367
Freyja <freyjalgy@gmail.com>
2022-12-11 15:10:05 +0100

    Création de classe de test AdminDAOTest

---
+++ code/ProjetColmar/src/main/test/Whole/daoPackage/AdminDAOTest.java
@@ -0,0 +1,14 @@
+package Whole.daoPackage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AdminDAOTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
