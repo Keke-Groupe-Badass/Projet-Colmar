7536788f6cd4546173aa391b2040c877e1b63b80
Kaputchino <andreasmulard@gmail.com>
2022-12-16 17:42:21 +0100

    docu

--- code/ProjetColmar/src/main/java/Whole/daoPackage/SuperAbstractDAO.java
+++ code/ProjetColmar/src/main/java/Whole/daoPackage/SuperAbstractDAO.java
@@ -4,6 +4,9 @@ import Whole.SingleConnection;
 
 import java.sql.Connection;
 
+/**
+ * Classe permettant la construction de tout les DAO, en obtenant l'instance de singleConnection
+ */
 public class SuperAbstractDAO {
     static Connection cn;
     public SuperAbstractDAO(String url, String login, String password) {
