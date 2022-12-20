cc130f99084c010037376cb5e7cd4fce5f983123
Kaputchino <andreasmulard@gmail.com>
2022-12-20 23:04:23 +0100

    module

--- code/CleanProject/src/main/java/module-info.java
+++ code/CleanProject/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example.cleanproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.junit.jupiter.api;
+    requires java.desktop;
 
 
     opens com.example.cleanproject to javafx.fxml;
