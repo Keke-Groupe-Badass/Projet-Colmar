3d004a91461a667c4710639e7fa7bbf18bc4a1e7
Kaputchino <andreasmulard@gmail.com>
2022-12-30 00:00:33 +0100

    partie 2 interface modifcation lettrine

--- code/CleanProject/src/main/java/Whole/FXPackage/FXModifierLettrineControleur.java
+++ code/CleanProject/src/main/java/Whole/FXPackage/FXModifierLettrineControleur.java
@@ -1,14 +1,16 @@
 package Whole.FXPackage;
 
+import Whole.Metadonnee;
 import Whole.ccmsPackage.Lettrine;
+import Whole.ccmsPackage.Tag;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
+
 import java.util.ResourceBundle;
 
 public class FXModifierLettrineControleur implements Initializable {
@@ -31,6 +33,35 @@ public class FXModifierLettrineControleur implements Initializable {
     Button annulerBtn;
     @FXML
     Button confirmerBtn;
+    @FXML
+    TextField tagTextField;
+    @FXML
+    Button ajouterTagBtn;
+    @FXML
+    TextField nomMetaTextField;
+    @FXML
+    TextField valeurMetaTextField;
+    @FXML
+    TextField uniteValeurMetaTextField;
+    @FXML
+    TextField descriptionMetaTextField;
+    @FXML
+    Button ajouterMetaBtn;
+    @FXML
+    CheckBox suppresionMeta;
+    @FXML
+    CheckBox suppresionTag;
+    @FXML
+    ListView<Metadonnee> metaListView;
+    @FXML
+    ListView<Tag> tagListView;
+
+    Boolean tagSupression;
+    Boolean metaSupression;
+
+
+
+
 
     public void valider(ActionEvent event) {
         ControleurFunctions.changeScene(event, "FxInterfaceLettrinesModifierValidation.fxml");
@@ -38,6 +69,18 @@ public class FXModifierLettrineControleur implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        pageTextField.setText(""+lettrine.getNbPage());
+        lienTextField.setText(lettrine.getLien());
+        ouvrageTextField.setText(""+lettrine.getOuvrage().getId());
+        createurTextField.setText(""+lettrine.getCreateur().getId());
+        plagiatTextField.setText(""+lettrine.getIdentique());
+        labelModifier.setText("Modification - Lettrine n°"+lettrine.getId());
 
+        for(Metadonnee meta:lettrine.getMetadonnees()){
+            metaListView.getItems().add(meta);
+        }
+        for(Tag tag:lettrine.getTags()){
+            tagListView.getItems().add(tag);
+        }
     }
 }
