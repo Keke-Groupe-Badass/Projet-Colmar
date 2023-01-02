cd5d51bca9450d9fd6d819c9ccd06bc522628fce
Kaputchino <andreasmulard@gmail.com>
2023-01-02 18:31:25 +0100

    Correction erreur superABstractDao ainsi que rajout d'une méthode pour lire l'image d'une lettrine

--- code/CleanProject/src/main/java/Whole/daoPackage/SuperAbstractDAO.java
+++ code/CleanProject/src/main/java/Whole/daoPackage/SuperAbstractDAO.java
@@ -7,7 +7,7 @@ import java.sql.Connection;
 /**
  * Classe permettant la construction de tout les DAO, en obtenant l'instance de singleConnection
  */
-public class SuperAbstractDAO {
+public abstract class SuperAbstractDAO {
     static Connection cn;
     public SuperAbstractDAO(String url, String login, String password) {
         this.cn= SingleConnection.getInstance(url,login,password);
