0d549d730157dcca27b251644c56c3bb4a9515af
Kaputchino <andreasmulard@gmail.com>
2023-01-03 19:12:31 +0100

    mineure

--- code/CleanProject/src/main/java/Whole/daoPackage/SuperAbstractDAO.java
+++ code/CleanProject/src/main/java/Whole/daoPackage/SuperAbstractDAO.java
@@ -8,7 +8,7 @@ import java.sql.Connection;
  * Classe permettant la construction de tous les DAO en obtenant l'instance de SingleConnection
  */
 public abstract class SuperAbstractDAO {
-    public static Connection cn;
+    protected static Connection cn;
     public SuperAbstractDAO(String url, String login, String password) {
         cn = SingleConnection.getInstance(url,login,password);
     }
