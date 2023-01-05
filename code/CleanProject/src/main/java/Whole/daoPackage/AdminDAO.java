5b909ccfbfa5f190c145a3de98a6af7cd19e5be9
Freyja <freyjalgy@gmail.com>
2023-01-05 13:55:33 +0100

    Checkstyle AdminDAO

--- code/CleanProject/src/main/java/Whole/daoPackage/AdminDAO.java
+++ code/CleanProject/src/main/java/Whole/daoPackage/AdminDAO.java
@@ -2,42 +2,53 @@ package Whole.daoPackage;
 
 import Whole.SingleConnection;
 import Whole.exportPackage.ExportTypeInterface;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
 import java.sql.*;
-
 import java.util.ArrayList;
 
 
 /**
- * Classe permettant à l'administrateur de gérer la base de donnée
+ * Classe permettant à l'administrateur de gérer la base de données.
  */
 public class AdminDAO extends SuperAbstractDAO {
-
+    /**
+     * Utilisateur de la BDD.
+     */
     private String utilisateur;
+    /**
+     * Mdp de la BDD.
+     */
     private String mdp;
+    /**
+     * Lien de la BDD.
+     */
     private String bdd;
-
+    /**
+     * Liste de tables.
+     */
     private ArrayList<String> listeTable;
 
     /**
-     * Constructeur de la classe
+     * Constructeur de la classe.
+     *
+     * @param url   url de la BDD
+     * @param login login de la BDD
+     * @param mdp   mdp de la BDD
      */
-    public AdminDAO(String url, String login, String password) {
-        super(url, login, password);
+    public AdminDAO(String url, String login, String mdp) {
+        super(url, login, mdp);
         this.utilisateur = login;
-        this.mdp = password;
+        this.mdp = mdp;
         this.bdd = url;
     }
 
     /**
-     * Permet de stocker dans un fichier la BDD
+     * Permet de stocker dans un fichier la BDD.
      *
-     * @param e le nom de la méthode d'export
+     * @param e    le nom de la méthode d'export
      * @param path le fichier où seront exportées les données
      * @return true si l'export a pu se faire, false sinon
      * @author Andreas
@@ -45,7 +56,7 @@ public class AdminDAO extends SuperAbstractDAO {
      * @see ExportTypeInterface
      */
     public Boolean exportDonnee(ExportTypeInterface e, String path) {
-        if(e==null){
+        if (e == null) {
             return false;
         }
         if (e.getName().equals("SQL")) {
@@ -55,10 +66,12 @@ public class AdminDAO extends SuperAbstractDAO {
     }
 
     /**
-     * Permet d'exporter les données aux formats non-sql
-     * @author Andreas
+     * Permet d'exporter les données aux formats non-sql.
+     *
      * @param path le fichier où seront exportées les données
+     * @param e    ExportTypeInterface
      * @return true si l'export a pu se faire, false sinon
+     * @author Andreas
      */
     private boolean exportNonSQL(String path, ExportTypeInterface e) {
         boolean b = true;
@@ -72,10 +85,11 @@ public class AdminDAO extends SuperAbstractDAO {
     }
 
     /**
-     * Permet d'exporter les données au format SQL
-     * @author Andreas
+     * Permet d'exporter les données au format SQL.
+     *
      * @param path le fichier où seront exportées les données
      * @return true si l'export a pu se faire, false sinon
+     * @author Andreas
      */
     private boolean sqlExport(String path) {
         String os = System.getProperty("os.name");
@@ -83,7 +97,8 @@ public class AdminDAO extends SuperAbstractDAO {
         if (os.contains("Windows")) {
             type = "cmd.exe";
         }
-        String[] cmd = {type, "exportSQL.sh", "src/main/shell/exportSQL.sh", utilisateur, mdp, bdd, path};
+        String[] cmd = {type, "exportSQL.sh", "src/main/shell/exportSQL.sh",
+                        utilisateur, mdp, bdd, path};
         try {
             Runtime.getRuntime().exec(cmd);
             return true;
@@ -93,27 +108,29 @@ public class AdminDAO extends SuperAbstractDAO {
     }
 
     /**
-     * Permet de transformer une table en une liste de liste, le premier niveau de liste représentant les lignes
-     * et le second niveau les colonnes, la première ligne est le nom des colonnes.
-     * @author Andreas
+     * Permet de transformer une table en une liste de liste, le premier niveau
+     * de liste représentant les lignes et le second niveau les colonnes, la
+     * première ligne est le nom des colonnes.
+     *
      * @param table la table que l'on souhaite exporter
      * @return true si tout c'est bien passé, false sinon
+     * @author Andreas
      */
-    private ArrayList<ArrayList<String>> getTableList(String table){
-        ArrayList<ArrayList<String>> list= new ArrayList<>();
+    private ArrayList<ArrayList<String>> getTableList(String table) {
+        ArrayList<ArrayList<String>> list = new ArrayList<>();
         try {
             Statement stmt = cn.createStatement();
 
-            ResultSet rs= stmt.executeQuery("SELECT * FROM "+table);
-            ResultSetMetaData md =  rs.getMetaData();
-            int size =md.getColumnCount();
-            for(int i=0;i<size;i++){
+            ResultSet rs = stmt.executeQuery("SELECT * FROM " + table);
+            ResultSetMetaData md = rs.getMetaData();
+            int size = md.getColumnCount();
+            for (int i = 0; i < size; i++) {
                 list.get(0).add(md.getColumnName(i));
             }
-            int row=1;
-            while(rs.next()){
-                for(int j=0;j<row;j++){
-                    for(int i=0;i<size;i++){
+            int row = 1;
+            while (rs.next()) {
+                for (int j = 0; j < row; j++) {
+                    for (int i = 0; i < size; i++) {
                         list.get(j).add((String) rs.getObject(i));
                     }
                 }
@@ -126,40 +143,45 @@ public class AdminDAO extends SuperAbstractDAO {
     }
 
     /**
-     * Permet de stocker dans un fichier les logs
-     * @author Andreas
-     * @param cn La connection à la base de données
+     * Permet de stocker dans un fichier les logs.
+     *
+     * @param cn   La connection à la base de données
      * @param file le fichier où seront exportés les logs
+     * @return true si l'exportation s'est faite, false sinon
+     * @author Andreas
      * @see SingleConnection
      */
-    public boolean exportLog(File file,Connection cn)  {
+    public boolean exportLog(File file, Connection cn) {
         try {
             Statement stmt = cn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT `text`,`date`,`userLogin` FROM `log`");
+            ResultSet rs = stmt.executeQuery("SELECT `text`,`date`,`userLogin` "
+                    + "FROM `log`");
             String str = "Hello";
             BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-            bw.write("Log exporté le "+new Date( System.currentTimeMillis()));
+            bw.write("Log exporté le " + new Date(System.currentTimeMillis()));
 
-            while(rs.next()){
+            while (rs.next()) {
                 bw.newLine();
-                str=rs.getString(2)+" par "+rs.getString(3)+": "+rs.getString(1);
+                str = rs.getString(2) + " par " + rs.getString(3)
+                        + ": " + rs.getString(1);
                 bw.write(str);
-
             }
             bw.close();
 
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.err.println("something went wrong with the database link");
         } catch (IOException e) {
-            System.err.println("something went wrong with the writing of the file");
+            System.err.println("something went wrong with the file's writing");
         }
         return false;
     }
 
     /**
-     * Supprime les logs de la BDD
-     * @author Andreas
+     * Supprime les logs de la BDD.
+     *
      * @param cn La connection à la base de données
+     * @return true si fonctionne correctement, false sinon
+     * @author Andreas
      * @see SingleConnection
      */
     public Boolean deleteLog(Connection cn) {
@@ -175,16 +197,20 @@ public class AdminDAO extends SuperAbstractDAO {
     }
 
     /**
-     * Ajoute au log un text
-     * @author Andreas
+     * Ajoute au log un text.
+     *
      * @param txt Le message à enregistrer
      * @return true si l'insertion peut se faire, false sinon
+     * @author Andreas
      * @see SingleConnection
      */
-    public Boolean writeLog(String txt){
+    public Boolean writeLog(String txt) {
         try {
             Statement st = cn.createStatement();
-            st.execute("INSERT INTO `log`( `text`, `date`, `userLogin`) VALUES('"+txt+"','"+new Date( System.currentTimeMillis())+"','"+ utilisateur +"'");
+            st.execute("INSERT INTO `log`( `text`, `date`, `userLogin`) "
+                    + "VALUES('" + txt + "','"
+                    + new Date(System.currentTimeMillis()) + "','"
+                    + utilisateur + "'");
             return true;
         } catch (SQLException e) {
             System.err.println("something went wrong with the database link");
