0d13035b731e99658652085aca7996fc97e96af8
Freyja <freyjalgy@gmail.com>
2023-01-05 22:07:11 +0100

    CheckStyle de tout ccmsPackage

--- code/CleanProject/src/main/java/Whole/ccmsPackage/CCMS.java
+++ code/CleanProject/src/main/java/Whole/ccmsPackage/CCMS.java
@@ -2,13 +2,15 @@ package Whole.ccmsPackage;
 
 
 /**
- * Interface permettant la généralisation dans la classe AbstractDAO
+ * Interface permettant la généralisation dans la classe AbstractDAO.
+ *
+ * @param <CCMS> objet CCMS
  * @see Whole.daoPackage.AbstractDAO
  */
 public interface CCMS <CCMS>{
     /**
+     * Permet de verifier si 2 objets sont exactement similaires.
      *
-     * Permet de verifier si 2 objets sont exactement similaires
      * @author Andreas
      * @param objet l'objet à comparer
      * @return renvoie true si les deux objets sont similaires, false sinon
