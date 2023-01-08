c5e267d83cb3a80aa4bd29f05263e76e6f5372f2
Kaputchino <andreasmulard@gmail.com>
2023-01-08 14:45:56 +0100

    correction bugs

--- code/CleanProject/src/main/java/Whole/daoPackage/LettrineDAO.java
+++ code/CleanProject/src/main/java/Whole/daoPackage/LettrineDAO.java
@@ -974,7 +974,10 @@ public class LettrineDAO extends AbstractDAO<Lettrine> {
         }
 
         else {
-            idSet = new TreeSet<>(taille.get(0));
+            idSet = new TreeSet<>();
+            if(taille.size()>0){
+                idSet = new TreeSet<>(taille.get(0));
+            }
         }
 
         for (int id : idSet) {
@@ -1251,5 +1254,53 @@ public class LettrineDAO extends AbstractDAO<Lettrine> {
             throw new RuntimeException(e);
         }
         return list;
+    }
+    public Ouvrage getOuvrage(int id){
+        PreparedStatement stmt= null;
+        Ouvrage ouvrage = new Ouvrage(id);
+
+        try {
+            stmt = cn.prepareStatement("SELECT * FROM `ouvrages` where idOuvrage =?");
+            stmt.setInt(1,id);
+            ResultSet rs =stmt.executeQuery();
+            if(rs.next()){
+                System.out.println(id+"lol");
+                ouvrage.setId(rs.getInt(1));
+                ouvrage.setTitre(rs.getString(2));
+                ouvrage.setDateEdition(rs.getInt(3));
+                ouvrage.setFormat(rs.getString(4));
+                ouvrage.setLien(rs.getString(5));
+                ouvrage.setResolution(rs.getString(6));
+                ouvrage.setCreditPhoto(rs.getString(7));
+                ouvrage.setReechantillonage(rs.getBoolean(8));
+                ouvrage.setCopyright(rs.getString(9));
+                ouvrage.setNbPage(rs.getInt(10));
+                ouvrage.setLieuImpression(rs.getString(11));
+                ouvrage.setImprimeur(getPersonne(rs.getInt(12)));
+                ouvrage.setLibraire(getPersonne(rs.getInt(13)));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return ouvrage;
+    }
+    public Personne getPersonne(int id){
+        PreparedStatement stmt= null;
+        try {
+            stmt = cn.prepareStatement("SELECT * FROM `personnes` "
+                    + "WHERE `idPersonne`=?");
+            stmt.setInt(1,id);
+            ResultSet rs =stmt.executeQuery();
+            Personne p = new Personne();
+            if(rs.next()){
+                p.setId(rs.getInt(1));
+                p.setNom(rs.getString(2));
+                p.setPrenom(rs.getString(3));
+                p.setNote(rs.getString(4));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
     }
 }
--- code/CleanProject/src/main/java/Whole/wordCloud/cloudWordGenerator.java
+++ code/CleanProject/src/main/java/Whole/wordCloud/cloudWordGenerator.java
@@ -13,26 +13,21 @@ import java.nio.file.Paths;
 
 public class cloudWordGenerator {
     public static void main(String[] args) {
-
         try {
             String texte = TagDAO.tagAndSize();
-
             String path = "src/main/wordcloud/text.txt";
             Files.write(Paths.get(path), texte.getBytes());
             ProcessBuilder process = new ProcessBuilder("python", "src/main/wordcloud/cloud.py").inheritIO();
             Process p = process.start();
             p.waitFor();
-
             BufferedReader Buffered_Reader = new BufferedReader(
                     new InputStreamReader(
                             p.getInputStream()
                     ));
             String Output_line = "";
-
             while ((Output_line = Buffered_Reader.readLine()) != null) {
                 System.out.println(Output_line);
             }
-
         }
         catch (IOException | InterruptedException i) {
             i.printStackTrace();
